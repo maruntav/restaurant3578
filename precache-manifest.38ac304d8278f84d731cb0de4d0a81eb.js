3cbc1494206a882de3fa4151fef31cb60248d82a
Osman Hussein <osman_990@hotmail.com>
2019-02-11 14:48:20 +0000

    Updates

---
+++ precache-manifest.38ac304d8278f84d731cb0de4d0a81eb.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d10bc1a00bf5ed5b7682607c32d294f",
+    "url": "/restaurant3578/static/media/cup-cakes.8d10bc1a.jpg"
+  },
+  {
+    "revision": "1c024358e5b94722647f",
+    "url": "/restaurant3578/static/css/main.3f64823e.chunk.css"
+  },
+  {
+    "revision": "812d879c7f061763bd12778461cbde59",
+    "url": "/restaurant3578/static/media/reserve-background.812d879c.jpg"
+  },
+  {
+    "revision": "3dccf714dfe54159afd9",
+    "url": "/restaurant3578/static/js/1.3dccf714.chunk.js"
+  },
+  {
+    "revision": "acb5720e72be9abbe5f5",
+    "url": "/restaurant3578/static/js/runtime~main.acb5720e.js"
+  },
+  {
+    "revision": "695e9017df6456a853e25d369ab91fed",
+    "url": "/restaurant3578/static/media/vegis.695e9017.jpg"
+  },
+  {
+    "revision": "eecd73edeebd7714c535fe7822830e93",
+    "url": "/restaurant3578/static/media/three-bowels.eecd73ed.jpg"
+  },
+  {
+    "revision": "1c024358e5b94722647f",
+    "url": "/restaurant3578/static/js/main.1c024358.chunk.js"
+  },
+  {
+    "revision": "888369e57d9bea4c28fee8c9006f06f5",
+    "url": "/restaurant3578/static/media/brown-bowel.888369e5.jpg"
+  },
+  {
+    "revision": "4a20b76a6c0539f468742acd0ef32bee",
+    "url": "/restaurant3578/static/media/meat_cook1.4a20b76a.mp4"
+  },
+  {
+    "revision": "40435c021a8a8ca7d2b989833bc94a89",
+    "url": "/restaurant3578/static/media/dessert.40435c02.jpg"
+  },
+  {
+    "revision": "d3a5de2c049298b34b9ce4fca9402e03",
+    "url": "/restaurant3578/static/media/full-menu.d3a5de2c.jpeg"
+  },
+  {
+    "revision": "b67997cd64fee69d815572e6f6b92574",
+    "url": "/restaurant3578/static/media/starter.b67997cd.jpg"
+  },
+  {
+    "revision": "3dccf714dfe54159afd9",
+    "url": "/restaurant3578/static/css/1.03ab8f6e.chunk.css"
+  },
+  {
+    "revision": "b478da845db64bd6e055040168c0e197",
+    "url": "/restaurant3578/index.html"
+  }
+];
